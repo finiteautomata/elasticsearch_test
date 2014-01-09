cb2dcf15abbc8cca72036d381aae39ac6a6e1684
jmperez <jmperez@devartis.com>
2014-01-09 14:27:28 -0300

    Modify search example.

--- shell_examples/request_search_examples.sh
+++ shell_examples/request_search_examples.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-echo "This is a query getting the titles from the two first chapters"
+echo "This is a query getting the titles from the all the chapters containing island in the text"
 URL='http://localhost:9200/darwin-origin/_search?pretty=true'
 
-curl $URL -XGET -d '{
-  "query": {
-      "match": {
-        "text": "ddisland"
-      }
+curl -XPOST $URL -d '{
+    "fields": ["title"],
+    "query": {
+        
+        "match": {
+            "text": "islands"
+        }
     }
 }'
