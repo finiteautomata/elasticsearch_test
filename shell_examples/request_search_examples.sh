4514a3464e3b36d60f2b6c26fe8fc88ce6363c48
jmperez <jmperez@devartis.com>
2014-01-09 14:53:27 -0300

    More request body searches.

--- shell_examples/request_search_examples.sh
+++ shell_examples/request_search_examples.sh
@@ -5,10 +5,72 @@ URL='http://localhost:9200/darwin-origin/_search?pretty=true'
 
 curl -XPOST $URL -d '{
     "fields": ["title"],
-    "query": {
-        
+    "query": {   
         "match": {
             "text": "islands"
         }
     }
+}'
+
+echo "==========================================="
+echo "This query get the titles in two pages of size 4"
+echo "First page"
+curl -XPOST $URL -d '{
+    "from": 0,
+    "size": 4,
+    "fields": ["title"],
+    "query": {   
+        "match": {
+            "text": "islands"
+        }
+    }
+
+}'
+
+echo "Second page"
+curl -XPOST $URL -d '{
+    "from": 4,
+    "size": 4,
+    "fields": ["title"],
+    "query": {   
+        "match": {
+            "text": "islands"
+        }
+    }
+
+}'
+
+echo "==========================================="
+echo "This query get all the titles of chapters that contain 'evolution' sorted by title"
+echo "Obs: This may not work as expected because of: For string based types, the field sorted on should not be analyzed / tokenized (http://www.elasticsearch.org/guide/en/elasticsearch/reference/master/search-request-sort.html)"
+
+curl -XPOST $URL -d '{
+    "fields": ["title"],
+    "sort": [
+        {"title" : {"order" : "asc"}}
+    ],
+    "query": {   
+        "match": {
+            "text": "evolution"
+        }
+    }
+
+}'
+
+echo "==========================================="
+echo "This query get all the titles and number of chapters that contain 'evolution' within a partial called 'data'"
+echo ""
+
+curl -XPOST $URL -d '{
+    "partial_fields" : {
+        "data":  {
+            "include": ["title", "numeral"]
+        } 
+    },
+    "query": {   
+        "match": {
+            "text": "evolution"
+        }
+    }
+
 }'
