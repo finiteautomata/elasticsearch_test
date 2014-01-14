e644838fdb5f20987e4af5487bebf373597aad6e
jmperez <jmperez@devartis.com>
2014-01-14 12:39:56 -0300

    Add another filter example.

--- shell_examples/filter_examples.sh
+++ shell_examples/filter_examples.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "This is a query getting a facet grouping by department_name from the products that contain 'network' in their names, and a facet taking"
-echo "The result is post-filtered so we only fetch the books. The facet, though, is still constructed with the original query"
+echo "The result is filtered so we only fetch the books. The facet, though, is still constructed with the original query"
 URL='http://localhost:9200/products/_search?pretty=true'
 
 curl -X POST "http://localhost:9200/products/_search?pretty=true" -d '{
@@ -19,10 +19,49 @@ curl -X POST "http://localhost:9200/products/_search?pretty=true" -d '{
         }
     },
 
-    "post_filter" : {
-        "term" : {
-            "department_name_analyzed": "book"
+    "filter" : {
+        "bool": {
+            "must": {
+                "term" : {
+                    "department_name_analyzed" : "book"
+                }   
+            }
+        }   
+    }
+}
+'
+echo "This is a query getting a facet grouping by department_name from the products that contain 'network' in their names, and a facet taking"
+echo "The result is filtered so we only fetch the books. The facet, though, is still constructed with the original query"
+URL='http://localhost:9200/products/_search?pretty=true'
+
+curl -X POST "http://localhost:9200/products/_search?pretty=true" -d '{
+
+    "facets" : {
+        "departments" : { 
+            "terms" : {
+                "field" : "department_name"
+            } 
         }
+    },
+
+    "query": {
+       "filtered" : {
+            "query": {
+                "match" : {
+                    "name" : "network"
+                }
+            },
+
+            "filter" : {
+                "bool": {
+                    "should": [
+                        { "term" : {"department_name" : "Books"}},
+                        { "term" : {"department_name" : "Electronics"}}
+                    ]            
+                }   
+            }
+        }
+
     }
 }
 '
